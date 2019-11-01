7c2725d60d9db97b320a6b56d325a6a39c9d6b0a
Dmytro Chebotarov <d.chebotarov@irri.org>
2019-11-01 14:09:29 +0800

    fix PCA file destination

--- makePCA.sh
+++ makePCA.sh
@@ -1,10 +1,15 @@
 
 root=$1
+outbase=pca_${root##*/}
+
 mkdir -p PCA
-plink --bfile $root  --keep-allele-order --pca 10 header --out PCA/pca_$root
+plink --bfile $root  --keep-allele-order --pca 10 header --out PCA/$outbase
 
+# add less PCA for plink GLM
 
-./pca2covar-gemma.sh PCA/pca_$root.eigenvec 1  | tr -s " " > PCA/pca_for_gemma.txt
+./pca2covar-gemma.sh PCA/$outbase.eigenvec 1  | tr -s " " > PCA/pca_for_gemma.txt
 
 cut -d" " -f1-6 PCA/pca_for_gemma.txt > PC1-5.txt
+
+# cut -d" " -f1-4 PCA/pca_for_gemma.txt > PC1-3.txt
 
